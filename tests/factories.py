bad6f62b691a783bc42cdc3212f725f1bfe7c179
Ujjval Chopra <ujjval2002chopra@gmail.com>
2025-06-14 23:15:51 +0000

    added customer database

--- tests/factories.py
+++ tests/factories.py
@@ -3,18 +3,19 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from service.models import Customer
 
 
-class YourResourceModelFactory(factory.Factory):
+class CustomerFactory(factory.Factory):
     """Creates fake pets that you don't have to feed"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = YourResourceModel
+        model = Customer
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
+    name = factory.Faker("name")
+    address = factory.Faker("address")
+    email = factory.Faker("email")
 
-    # Todo: Add your other attributes here...
