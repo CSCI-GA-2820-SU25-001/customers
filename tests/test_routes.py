51aa53faf76885c863d3d8da8466c21efe7dfa64
Rachel Ren <rachelren2025@gmail.com>
2025-06-19 06:38:14 +0000

    Added create customer endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -25,10 +25,12 @@ from unittest import TestCase
 from wsgi import app
 from service.common import status
 from service.models import db, Customer
+from .factories import CustomerFactory
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
 )
+BASE_URL = "/customers"
 
 
 ######################################################################
@@ -71,3 +73,33 @@ class TestYourResourceService(TestCase):
         """It should call the home page"""
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_create_customer(self):
+        """It should Create a new Customer"""
+        test_customer = CustomerFactory()
+        logging.debug("Test Customer: %s", test_customer.serialize())
+        response = self.client.post(BASE_URL, json=test_customer.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = response.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_customer = response.get_json()
+        self.assertEqual(new_customer["first_name"], test_customer.first_name)
+        self.assertEqual(new_customer["last_name"], test_customer.last_name)
+        self.assertEqual(new_customer["address"], test_customer.address)
+        self.assertEqual(new_customer["phone_number"], test_customer.phone_number)
+        self.assertEqual(new_customer["email"], test_customer.email)
+
+        # # Todo: Uncomment this code when accounts is implemented
+        # # Check that the location header was correct
+        # response = self.client.get(location)
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # new_customer = response.get_json()
+        # self.assertEqual(new_customer["first_name"], test_customer.first_name)
+        # self.assertEqual(new_customer["last_name"], test_customer.last_name)
+        # self.assertEqual(new_customer["address"], test_customer.address)
+        # self.assertEqual(new_customer["phone_number"], test_customer.phone_number)
+        # self.assertEqual(new_customer["email"], test_customer.email)
