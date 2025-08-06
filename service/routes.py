458ec81081d1bd6947797f6c14ef06c9671b8e6a
Ujjval Chopra <ujjval2002chopra@gmail.com>
2025-08-06 03:47:56 +0000

    added route change

--- service/routes.py
+++ service/routes.py
@@ -233,7 +233,7 @@ class CustomerCollection(Resource):
             query = query.filter(Customer.email == email)
         if first_name:
             app.logger.info("Find by first_name (partial, case-insensitive): %s", first_name)
-            query = query.filter(Customer.first_name.ilike(f"%{first_name}%"))
+            query = query.filter(Customer.first_name == first_name)
         if last_name:
             app.logger.info("Find by last_name (partial, case-insensitive): %s", last_name)
             query = query.filter(Customer.last_name.ilike(f"%{last_name}%"))
